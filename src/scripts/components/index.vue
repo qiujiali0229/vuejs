ecfd63720a2f7271adac31cc142ce3636078b00c
unknown <lijunmin68@163.com>
2017-01-06 15:03:53 +0800

    修改了footer路由

--- src/scripts/components/index.vue
+++ src/scripts/components/index.vue
@@ -3,31 +3,43 @@
 		 <router-view></router-view>
 		 <footer>
 		  	<ul>
-		  		<li>
-		  			<router-link to="/">
-		  				<i class="yo-ico">&#xe654;</i>
-		  			    <b>精选</b>
-		  			</router-link>
-		  		</li>
-		  		<li>
-		  			<router-link to="/search">
-			  			<i class="yo-ico">&#xe604;</i>
-			  			<b>发现</b>
-		  			</router-link>
-		  		</li>
-		  		<li>
-		  			<router-link to="/supermarket">
-			  			<i class="yo-ico">&#xe675;</i>
-			  			<b>商城</b>
-		  			</router-link>
-		  		</li>
-		  		<li>
-		  			<router-link to="/my">
-			  			<i class="yo-ico">&#xe66f;</i>
-			  			<b>个人</b>
+		  		<li v-for="fn in footerNav">
+		  			<router-link :to="fn.router">
+		  				<i class="yo-ico"v-html="fn.ico"></i>
+		  			    <b>{{fn.title}}</b>
 		  			</router-link>
 		  		</li>
 		  	</ul>
 		 </footer>
 	</div>
 </template>
+<script>
+	module.exports = {
+		data:function (){
+			return {
+				footerNav:[
+					{
+						router:'/',
+						ico:'&#xe654;',
+						title:'首页'
+					},
+					{
+						router:'/search',
+						ico:'&#xe604;',
+						title:'发现'
+					},
+					{
+						router:'/supermarket',
+						ico:'&#xe675;',
+						title:'商城'
+					},
+					{
+						router:'/my',
+						ico:'&#xe66f;',
+						title:'我的'
+					},
+				]
+			}
+		}
+	}
+</script>
